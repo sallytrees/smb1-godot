578168108667cc9420e954896e8b3fe6a4c93357
sallytrees <sallytrees@proton.me>
2025-12-14 04:02:54 +0000

    Delete mario-bros directory

--- mario-bros/entitys/objects/coins/coin.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _physics_process(delta: float) -> void:
-	$AnimatedSprite2D.play("shin")
-func _on_collect_body_entered(body: Node2D) -> void:
-	queue_free()
--- mario-bros/entitys/player/area_2d.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-signal ACTIVATE
-
-
-func _on_body_entered(body: Node2D) -> void:
-	emit_signal("ACTIVATE")
